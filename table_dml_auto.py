eff442c2976649a415491319fea27a0f974273f6
Debasish <53166394+dghosh1234@users.noreply.github.com>
2025-07-30 02:37:17 -0400

    Update table_dml_auto.py

---
+++ table_dml_auto.py
@@ -0,0 +1,10 @@
+The concept allows you to create multiple test cases from the source table. Each test case is defined by a group name (e.g., TEST_CASE_1, but any name can be used).
+
+For each group, you can specify:
+
+A separate WHERE condition for the INSERT record set
+
+A separate WHERE condition for the UPDATE record set
+
+
+This flexibility enables you to simulate various scenarios by controlling which source records are selected for insertion or update into the target tabl
