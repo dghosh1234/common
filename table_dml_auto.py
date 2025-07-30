1dfb9c72340fd09c552f699504c996a7b059ff81
Debasish <53166394+dghosh1234@users.noreply.github.com>
2025-07-30 03:16:30 -0400

    Update table_dml_auto.py

---
+++ table_dml_auto.py
@@ -0,0 +1,7 @@
+Please find attached two documents that describe the steps required to generate UPSERT statements to support ADTF test case framework.
+
+This code currently does not cover all possible scenarios, but it can serve as a starting point to minimize manual effort when creating DML statements.
+
+I request you to review and use it over a couple of days with different use cases before providing feedback. This will help ensure your comments are more comprehensive and practical.
+
+For any further clarification, feel free to reach out to me directly.
