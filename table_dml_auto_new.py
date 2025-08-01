c5460ce2bc92180ca3918fbe941aed3508432022
Debasish <53166394+dghosh1234@users.noreply.github.com>
2025-07-31 21:00:25 -0400

    Update table_dml_auto_new.py

--- table_dml_auto_new.py
+++ table_dml_auto_new.py
@@ -2,20 +2,19 @@ Option Explicit
 
 Dim tbl, col
 Dim output
-output = "Inspecting VARCHAR2 columns for length semantics:" & vbCrLf & vbCrLf
+output = "Inspecting all columns:" & vbCrLf & vbCrLf
 
 For Each tbl In ActiveModel.Tables
     If Not tbl.IsShortcut Then
         output = output & "Table: " & tbl.Code & vbCrLf
         For Each col In tbl.Columns
-            If UCase(col.DataType) = "VARCHAR2" Then
-                output = output & "  Column: " & col.Code & vbCrLf
-                output = output & "    Length: " & col.Length & vbCrLf
-                output = output & "    LengthSemantics: '" & col.LengthSemantics & "'" & vbCrLf
-            End If
+            output = output & "  Column: " & col.Code & vbCrLf
+            output = output & "    DataType: '" & col.DataType & "'" & vbCrLf
+            output = output & "    Length: " & col.Length & vbCrLf
+            output = output & "    LengthSemantics: '" & col.LengthSemantics & "'" & vbCrLf
         Next
         output = output & vbCrLf
     End If
 Next
 
-MsgBox output, vbOKOnly, "DEBUG: VARCHAR2 Column Semantics"
+MsgBox output, vbOKOnly, "DEBUG: Column DataTypes and Semantics"
