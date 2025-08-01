148746e233ef5e9a090bd295a9a2522f31e3d72c
Debasish <53166394+dghosh1234@users.noreply.github.com>
2025-07-31 22:52:00 -0400

    Update table_dml_auto.py

--- table_dml_auto.py
+++ table_dml_auto.py
@@ -1,1 +1,30 @@
+Option Explicit
+
+Dim tbl, col
+Dim output
+Dim colMeta
+
+output = "Inspecting all columns:" & vbCrLf & vbCrLf
+
+For Each tbl In ActiveModel.Tables
+    If Not tbl.IsShortcut Then
+        output = output & "Table: " & tbl.Code & vbCrLf
+        For Each col In tbl.Columns
+            output = output & "  Column: " & col.Code & vbCrLf
+            output = output & "    DataType: '" & col.DataType & "'" & vbCrLf
+            output = output & "    Length: " & col.Length & "'" & vbCrLf
+
+            ' Check if LengthSemantics is supported
+            Set colMeta = col.GetExtendedAttribute("LengthSemantics", False)
+            If Not colMeta Is Nothing Then
+                output = output & "    LengthSemantics: '" & col.LengthSemantics & "'" & vbCrLf
+            Else
+                output = output & "    LengthSemantics: [N/A]" & vbCrLf
+            End If
+        Next
+        output = output & vbCrLf
+    End If
+Next
+
+MsgBox output, vbOKOnly, "DEBUG: Column DataTypes and Semantics"
 
