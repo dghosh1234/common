9ae838f90e649f759e5cc6dc615da9a7a7272d4a
Debasish <53166394+dghosh1234@users.noreply.github.com>
2025-08-01 01:14:51 -0400

    Update table_dml_auto.py

--- table_dml_auto.py
+++ table_dml_auto.py
@@ -2,7 +2,7 @@ Option Explicit
 
 Dim tbl, col
 Dim fso, file, filePath
-Dim semVal, suggestion, lineOut
+Dim dataTypeRaw, semantics, suggestion, len, lineOut
 
 ' File path on desktop
 filePath = CreateObject("WScript.Shell").SpecialFolders("Desktop") & "\varchar2_semantics_report.csv"
@@ -15,23 +15,27 @@ file.WriteLine "Table Name,Column Name,Data Type,Length,Semantics,Suggestion"
 For Each tbl In ActiveModel.Tables
     If Not tbl.IsShortcut Then
         For Each col In tbl.Columns
-            If InStr(UCase(col.DataType), "VARCHAR2") > 0 Then
-                Dim dataTypeUpper, len
-                dataTypeUpper = UCase(col.DataType)
+            dataTypeRaw = UCase(col.DataType)
+            
+            If InStr(dataTypeRaw, "VARCHAR2") > 0 Then
+                ' Default values
                 len = col.Length
+                semantics = ""
+                suggestion = ""
 
-                ' Get semantics from extended attribute (CHAR/BYTE/blank)
-                On Error Resume Next
-                semVal = col.GetExtendedAttribute("LengthSemantics")
-                If Err.Number <> 0 Then semVal = "" : Err.Clear
-                On Error GoTo 0
+                ' Detect semantics from data type
+                If InStr(dataTypeRaw, "CHAR") > 0 Then
+                    semantics = "CHAR"
+                ElseIf InStr(dataTypeRaw, "BYTE") > 0 Then
+                    semantics = "BYTE"
+                Else
+                    semantics = ""  ' Means BYTE is implicit
+                End If
 
-                semVal = UCase(Trim(semVal))
-
-                ' ✅ Include only if semantics is BYTE or blank AND not already using VARCHAR2(n CHAR)
-                If (semVal = "BYTE" Or semVal = "") And InStr(dataTypeUpper, "CHAR") = 0 Then
-                    suggestion = "Change to VARCHAR2(" & len & " CHAR) semantics"
-                    lineOut = """" & tbl.Code & """,""" & col.Code & """,""" & col.DataType & """,""" & len & """,""" & semVal & """,""" & suggestion & """"
+                ' If not already CHAR, suggest changing
+                If semantics <> "CHAR" Then
+                    suggestion = "Change it to VARCHAR2(" & len & " CHAR)"
+                    lineOut = """" & tbl.Code & """,""" & col.Code & """,""" & col.DataType & """,""" & len & """,""" & semantics & """,""" & suggestion & """"
                     file.WriteLine lineOut
                 End If
             End If
